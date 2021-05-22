b83022256c167dab61418200590a1fc04583eef8
Elvinas V <elvinas.val@gmail.com>
2021-05-22 18:27:17 +0300

    New Tweet reducer

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,16 @@
-import {saveLikeToggle} from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+import { saveLikeToggle, saveTweet } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
+
+function addTweet(tweet){
+    return {
+        type: ADD_TWEET,
+        tweet,
+    }
+}
 
 export function receiveTweets(tweets) {
     return {
@@ -15,6 +25,22 @@ const toggleTweet = ({id, authedUser, hasLiked}) => {
         id,
         authedUser,
         hasLiked
+    }
+}
+
+export function handleAddTweet(tweet, replyingTo){
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+
+        dispatch(showLoading())
+
+        saveTweet({
+            text: tweet,
+            author: authedUser,
+            replyingTo
+        })
+            .then((tweet) => dispatch(addTweet(tweet)))
+            .then(() => dispatch(hideLoading()))
     }
 }
 
