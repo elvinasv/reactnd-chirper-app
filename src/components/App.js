4f570b1a44ef3a972c397b00c2bdcb96f5134896
Elvinas V <elvinas.val@gmail.com>
2021-05-22 20:02:41 +0300

    Add Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import NewTweet from './NewTweet'
 import TweetPage from './TweetPage'
+import Nav from './Nav'
 class App extends Component {
     componentDidMount(){
         this.props.dispatch(handleInitialData())
@@ -12,10 +14,21 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <Router>
+                <Fragment>
                 <LoadingBar />
-                {!this.props.loading && <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>}
-            </div>
+                <div className="container">
+                    <Nav />
+                    {!this.props.loading && (
+                        <div>
+                            <Route path='/' exact component={Dashboard}/>
+                            <Route path='/tweet/:id' component={TweetPage}/>
+                            <Route path='/new' component={NewTweet}/>
+                        </div>
+                    )}
+                </div>
+                </Fragment>
+            </Router>
         )
     }
 }
